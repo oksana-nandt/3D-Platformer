27753639b460c6d85711f5a949fc6bab3b7ad6f0
Oksana Nandt <83062854+oksana-nandt@users.noreply.github.com>
2022-11-18 12:34:48 +0500

    Add files via upload

---
+++ Missile.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Missile : MonoBehaviour
+{
+    //Скорость полёта огненного шара
+    public float speed;
+
+    void Update()
+    {
+        //Каждый кадр позиция шара обновляется на результат умножения вектора движния вперёд (0,0,1) и скорости
+        transform.position += transform.forward * speed * Time.deltaTime;
+    }
+
+    void OnTriggerEnter(Collider other) {
+
+        //Враг уничтожается
+        Enemy enemy = other.GetComponent<Enemy>();
+        Destroy(enemy.gameObject);
+
+        //Снаряд уничтожается
+        Destroy(gameObject);
+
+    }
+
+}
