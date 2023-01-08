f2789d40e7f90867fa260d42eff38624e0f569fd
Oksana Nandt <83062854+oksana-nandt@users.noreply.github.com>
2023-01-08 20:57:35 +0500

    Add files via upload

--- Player.cs
+++ Player.cs
@@ -1,32 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Player : MonoBehaviour
 {
+    //Текущее здоровье игрока
+    public int health = 10;
     //Максимальное здоровье игрока
-    private int health = 10;
+    public int maxHealth = 10;
 
     //Число собранных монет
-    private int coins;
+    public int coins;
 
-    //Префаб огненнего шара и параметр Transform точки атаки
+    //Префаб огненного шара и параметр Transform точки атаки
     public GameObject fireballPrefab;
     public Transform attackPoint;
 
+    //Компонент, отвечающий за проигрывание звуков
+    public AudioSource audioSource;
 
-    //Метод, понижающий здоровье игрока
+    //Звуковой файл, содержащий звуковой эффект нанесения урона
+    public AudioClip damageSound;
+
+    //Метод, обрабатывающий нанесённый урон
     public void TakeDamage(int damage)
     {
         health -= damage;
-        print("Здоровье игрока: " + health);
+
+        //Если здоровье ещё есть, то проигрывается звук нанесения урона
+        if(health > 0)
+        {
+            audioSource.PlayOneShot(damageSound);
+            //print("Здоровье игрока: " + health);
+        }
+        //Если здоровья нет, то перезапускается текущая сцена
+        else
+        {
+            int sceneIndex = SceneManager.GetActiveScene().buildIndex;
+            SceneManager.LoadScene(sceneIndex);
+        }
     }
 
     //Метод, увеличивающий число монеток
     public void CollectCoins()
     {
         coins++;
-        print("Собранные монетки: " + coins);
+        //print("Собранные монетки: " + coins);
     }
 
 
